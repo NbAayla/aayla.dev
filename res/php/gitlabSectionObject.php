b24cc7769bcb8be212f05a7d0949af2afb78e667
afetzer <aayla@aayla.dev>
2020-07-26 01:09:58 -0600

    Fixed gitlab links

--- res/php/gitlabSectionObject.php
+++ res/php/gitlabSectionObject.php
@@ -1,6 +1,6 @@
 <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5" id="gitlab-template">
     <div class="resume-content">
-        <h3 class="mb-0"><a href="<?php echo $data["id"]; ?>"><?php echo $data["name"]; ?></a></h3>
+        <h3 class="mb-0"><a href="<?php echo $data["web_url"]; ?>"><?php echo $data["name_with_namespace"]; ?></a></h3>
         <div class="subheading mb-3"><?php echo $data["description"]; ?></div>
     </div>
 </div>
