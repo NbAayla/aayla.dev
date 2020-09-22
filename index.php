60ad6257c2ccec55a8563e81ebcdd7655c7c6fe6
aaylafetzer <aayla@aayla.dev>
2020-09-21 22:14:17 -0600

    Brought experienceObject.php into index.php

--- index.php
+++ index.php
@@ -122,7 +122,30 @@ foreach ($jsonContent["Sections"] as $section => $value) {
     switch ($value["Type"]) {
         case "Experience":
             foreach ($value["Content"] as $key => $value) {
-                include("res/php/experienceObject.php");
+                echo '
+                <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="resume-content">';
+                if (array_key_exists("Title", $value)) {
+                    echo '<h3 class="mb-0">' . $value["Title"] . '</h3>';
+                }
+                if (array_key_exists("Subtitle", $value)) {
+                    echo '<div class="subheading mb-3">' . $value["Subtitle"] . '</div>';
+                }
+                if (array_key_exists("Roles", $value)) {
+                    echo '<ul>';
+                    foreach ($value["Roles"] as $rolesKey => $rolesValue) {
+                        $rolesValue = Markdown::defaultTransform($rolesValue);
+                        echo "<li>" . strip_tags($rolesValue, '<a>') . "</li>";
+                    }
+                    echo '</ul>';
+                }
+                if (array_key_exists("Date Range", $value)) {
+                    echo '
+                                </div><div class="resume-date text-md-right">
+                                    <span class="text-primary">' . $value["Date Range"] . '</span>
+                                </div>';
+                }
+                echo '</div>';
             }
             break;
         case "Links":
