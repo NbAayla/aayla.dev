ff1638554173984bdcffdf312292e6fbf296e783
aaylafetzer <aayla@aayla.dev>
2020-09-21 21:25:48 -0600

    Start re-write

--- index.php
+++ index.php
@@ -1,115 +1,64 @@
 <?php
-    $jsonFile = fopen("en.json", "r") or die("Unable to read JSON content!");
-    $jsonString = fread($jsonFile, filesize("en.json"));
-    fclose($jsonFile);
-    $jsonContent = json_decode($jsonString, true);
-    $splitName = explode(" ", $jsonContent["Name"]);
-
-    set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
-        // error was suppressed with the @-operator
-        if (0 === error_reporting()) {
-            return false;
-        }
-
-        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
-    });
-
-    try {
-        include_once("res/php/php-markdown/Michelf/Markdown.inc.php");
-    } catch (Exception $e) {
-        shell_exec("/usr/bin/git submodule update --init --recursive");
+set_error_handler(function($errno, $error_str, $error_file, $error_line) {
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
     }
+    throw new ErrorException($error_str, 0, $errno, $error_file, $error_line);
+});
+
+// Import requirements
+try {
     include_once("res/php/php-markdown/Michelf/Markdown.inc.php");
-    use Michelf\Markdown;
-?>
-<html lang="en">
+} catch (Exception $e) {
+    shell_exec("/usr/bin/git submodule update --init --recursive");
+}
+include_once("res/php/php-markdown/Michelf/Markdown.inc.php");
+use Michelf\Markdown;
 
-<head>
+// Load page content
+$jsonContent = json_decode(
+    file_get_contents("content.json"), true
+);
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+// Start the file
+echo '
+<html lang=" ' . $jsonContent["Meta"]["Language"] . '">
+<head><title>' . $jsonContent["Meta"]["Title"] . '</title>
+';
 
-    <?php
-    include("res/php/metaTags.php");
-    ?>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha256-YLGeXaapI0/5IgZopewRJcFXomhRMlYYjugPLSyNjTY=" crossorigin="anonymous" />
+// Get URL for profile image
+$imageUrl = $jsonContent["Meta"]["URL"] . "/" . $jsonContent["Image"];
 
-  <!-- Custom fonts for this template -->
-  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha256-UzFD2WYH2U1dQpKDjjZK72VtPeWP50NoJjd26rnAdUI=" crossorigin="anonymous" />
+// Create slightly different meta tags for each platform because the market system fosters competition that generally
+// produces the most efficient allocation of resources.
+$metaTags = array(
+    $jsonContent["Meta"]["Title"] => array(["title", "og:title", "twitter:title"]),
+    $jsonContent["Bio"] => array(["description", "og:description", "twitter:description"]),
+    $jsonContent["Meta"]["URL"] => array(["og:url", "twitter:url"]),
+    $imageUrl => array(["og:image", "twitter:image"]),
+    "summary_large_image" => array(["twitter:card"])
+);
+// Create the meta tags
+foreach ($metaTags as $value => $tags) {
+    foreach (array_values($tags) as $tag_array) {
+        foreach($tag_array as $tag) {
+            echo '<meta property="' . $tag .'" content="' . $value . '">';
+        }
+    }
+}
 
-  <!-- Custom styles for this template -->
-  <link href="/res/css/resume.css" rel="stylesheet">
+// Build rest of head and start of body
+echo '
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha256-YLGeXaapI0/5IgZopewRJcFXomhRMlYYjugPLSyNjTY=" crossorigin="anonymous" />
+<!-- Custom fonts for this template -->
+<link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha256-UzFD2WYH2U1dQpKDjjZK72VtPeWP50NoJjd26rnAdUI=" crossorigin="anonymous" />
+<!-- Custom styles for this template -->
+<link href="/res/css/resume.css" rel="stylesheet">
 </head>
-
 <body id="page-top">
+';
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-    <a class="navbar-brand js-scroll-trigger" href="#page-top">
-      <span class="d-block d-lg-none"><?= $jsonContent["Name"]; ?></span>
-      <span class="d-none d-lg-block">
-        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="/res/img/profile.jpg" alt="">
-      </span>
-    </a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav">
-          <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#page-top">About</a>
-          </li>
-          <?php
-            foreach ($jsonContent["Sections"] as $section => $key) {
-                echo "<li class=\"nav-item\">";
-                echo "<a class=\"nav-link js-scroll-trigger\" href=\"#" . $key["Anchor"] . "\">$section</a>";
-                echo "</li>";
-            }
-          ?>
-      </ul>
-    </div>
-  </nav>
-
-  <div class="container-fluid p-0">
-    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
-      <div class="w-100">
-        <h1 class="mb-0"><?php echo $splitName[0]; ?>
-          <span class="text-primary"><?php echo $splitName[1]; ?></span>
-        </h1>
-        <div class="subheading mb-5">
-          <a href="<?php echo $jsonContent["Email"]; ?>" aria-hidden="true"><?php echo $jsonContent["Email"]; ?></a>
-        </div>
-        <p class="lead mb-5">
-            <?php
-                if ($jsonContent["Bio"]) {
-                    echo $jsonContent["Bio"];
-                }
-            ?>
-        </p>
-        <?php
-        if ($jsonContent["Social"]) {
-            include("res/php/socialIcons.php");
-        }
-        ?>
-      </div>
-    </section>
-
-      <?php
-        foreach ($jsonContent["Sections"] as $section => $value) {
-            switch ($value["Type"]) {
-                case "Experience":
-                    include("res/php/experienceSection.php");
-                    break;
-                case "Links":
-                    include("res/php/linkSection.php");
-                    break;
-                case "Gitlab":
-                    include("res/php/gitlabSection.php");
-                    break;
-                case "Github":
-                    include("res/php/githubSection.php");
-            }
-        }
-      ?>
-
-  </div>
-</body>
-</html>
+// Build
--- res/php/metaTags.php
+++ res/php/metaTags.php
@@ -1,12 +1,10 @@
-<!-- Primary Meta Tags -->
-<title><?php echo $jsonContent["Name"]; ?> - Resume</title>
-<meta name="title" content="<?= $jsonContent["Name"]; ?> - Resume">
-<meta name="description" content="<?= $jsonContent["Bio"]; ?>">
 
-<!-- Open Graph / Facebook -->
+
+
+
 <meta property="og:type" content="website">
-<meta property="og:url" content="https://afetzer.com">
-<meta property="og:title" content="<?= $jsonContent["Name"]; ?> - Resume">
+<meta property="og:url" content="' . $jsonContent["Meta"]["URL"] . '">
+<meta property="og:title" content="' . $jsonContent["Meta"]["Title"] . '">
 <meta property="og:description" content="<?= $jsonContent["Bio"]; ?>">
 <meta property="og:image" content="https://afetzer.com/res/img/profile.jpg">
 
