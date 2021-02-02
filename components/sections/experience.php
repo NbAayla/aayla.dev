da0524105bfd3eb958edf56361384f5157bdaff7
Aayla Fetzer <aayla@aayla.dev>
2021-02-02 11:44:47 -0700

    Added nord theme

--- components/sections/experience.php
+++ components/sections/experience.php
@@ -1,7 +1,7 @@
 <hr class="m-0">
 <section id="<?php echo slugify($section); ?>" class="resume-section p-3 p-lg-5 d-flex align-items-center">
 <div class="w-100">
-<h2 class="mb-5"><?php echo $section; ?></h2>
+<h2 class="mb-5m"><?php echo $section; ?></h2>
 
 <?php
 use Michelf\Markdown;
