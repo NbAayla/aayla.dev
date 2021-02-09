f9ae52c73fd67eef1bb192be4a830c8aedfd3216
Aayla Fetzer <aayla@aayla.dev>
2021-02-08 19:05:40 -0700

    Added subheader

--- components/splash.php
+++ components/splash.php
@@ -2,9 +2,20 @@
 <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
     <div class="w-100">
         <h1 class="mb-0"><?php echo $yamlContent->name; ?></h1>
-        <div class="subheading mb-5">
-            <a href="mailto:<?php echo $yamlContent->email; ?>" aria-hidden="true"><?php echo $yamlContent->email; ?></a>
-        </div>
+        <? 
+        use Michelf\Markdown;
+        if ($yamlContent->subheading) {
+            echo "<div class='subheading mb-5'>";
+            foreach ($yamlContent->subheading as $key=>$value) {
+                echo substr(Markdown::defaultTransform($value), 3, -5);
+                // Echo a middle dot to separate the elements on all but the last entry to prevent a trailing dot
+                if ($key != array_key_last((array) $yamlContent->subheading)) {
+                    echo " &middot ";
+                }
+            }
+            echo "</div>";
+        }
+        ?>
         <p class="lead mb-5"><?php echo $yamlContent->bio; ?></p>
 
 <?php
