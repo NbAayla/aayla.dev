a6abfe5a85eadf602e71ec73753150324564110e
Aayla Fetzer <aayla@aayla.dev>
2021-02-08 21:42:50 -0700

    CSS optimization broke mobile

--- components/head.php
+++ components/head.php
@@ -31,7 +31,8 @@ foreach ($metaTags as $value => $tags) {
     */
 ?>
 
-<link rel="stylesheet" href="/res/css/vendor/bootstrap.css">
+<!-- <link rel="stylesheet" href="/res/css/vendor/bootstrap.css"> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" />
 <link rel="stylesheet" href="/res/css/vendor/fontawesome.css">
 <!-- Custom fonts -->
 <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
