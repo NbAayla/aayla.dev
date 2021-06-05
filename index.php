93eeb4214d78dc603e4a7880042c97c16123a0f6
Aayla Semyonova <aayla@aayla.dev>
2021-06-04 18:33:50 -0600

    Renamed

--- index.php
+++ index.php
@@ -1,30 +1,214 @@
-<?php
-require_once 'vendor/autoload.php';
+<!DOCTYPE html>
+<html class="no-js" lang="">
+    <head>
+        <!-- meta charset -->
+        <meta charset="utf-8">
+        <!-- site title -->
+        <title>Phantom | Personal Resume Template</title>
+        <!-- meta description -->
+        <meta name="description" content="">
+        <!-- mobile viwport meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- fevicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+        
+        <!-- ================================
+        CSS Files
+        ================================= -->
+        <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i|Open+Sans:400,600,700,800" rel="stylesheet">
+        <link rel="stylesheet" href="res/css/themefisher-fonts.min.css">
+        <link rel="stylesheet" href="res/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="res/css/bootstrap.min.css">
+        <link rel="stylesheet" href="res/css/main.css">
+        <link rel="stylesheet" href="res/css/dark.css">
+        <link id="color-changer" rel="stylesheet" href="res/css/colors/blue.css">
+    </head>
 
-use \Dallgoot\Yaml;
+    <body class="dark">
 
-// Load content from content.yml
-$yamlContent = Yaml::parseFile("content.yml", 0, 0);
+        <main class="site-wrapper">
+            <div class="pt-table">
+                <div class="pt-tablecell page-home relative" style="background-image: url('img/banner.jpg');">
+                    <div class="overlay"></div>
 
-// Include functions
-include "res/php/functions.php";
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-2 col-lg-8">
+                                <div class="page-title home text-center">
+                                    <h2>Aayla <span class="primary">Semyonova</span></h2>
+                                    <p>A 20 year-old Rust, Python, and Web developer <b>&middot;</b> she/her</p>
+                                </div>
 
-// Render page head
-include "components/head.php";
+                                <div class="hexagon-menu clear">
+                                    <div class="hexagon-item">
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <a href="welcome.html" class="hex-content">
+                                            <span class="hex-content-inner">
+                                                <span class="icon">
+                                                    <i class="tf-dial"></i>
+                                                </span>
+                                                <span class="title">Welcome</span>
+                                            </span>
+                                            <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
+                                        </a>
+                                    </div>
+                                    <div class="hexagon-item">
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <a href="about.html" class="hex-content">
+                                            <span class="hex-content-inner">
+                                                <span class="icon">
+                                                    <i class="tf-profile-male"></i>
+                                                </span>
+                                                <span class="title">About</span>
+                                            </span>
+                                            <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
+                                        </a>
+                                    </div>
+                                    <div class="hexagon-item">
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <a href="services.html" class="hex-content">
+                                            <span class="hex-content-inner">
+                                                <span class="icon">
+                                                    <i class="tf-tools-2"></i>
+                                                </span>
+                                                <span class="title">Services</span>
+                                            </span>
+                                            <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
+                                        </a>    
+                                    </div>
+                                    <div class="hexagon-item">
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <a href="resume.html" class="hex-content">
+                                            <span class="hex-content-inner">
+                                                <span class="icon">
+                                                    <i class="tf-tools"></i>
+                                                </span>
+                                                <span class="title">Resume</span>
+                                            </span>
+                                            <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
+                                        </a>
+                                    </div>
+                                    <div class="hexagon-item">
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <a href="works.html" class="hex-content">
+                                            <span class="hex-content-inner">
+                                                <span class="icon">
+                                                    <i class="tf-briefcase2"></i>
+                                                </span>
+                                                <span class="title">Works</span>
+                                            </span>
+                                            <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
+                                        </a>
+                                    </div>
+                                    <div class="hexagon-item">
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <a href="testimonials.html" class="hex-content">
+                                            <span class="hex-content-inner">
+                                                <span class="icon">
+                                                    <i class="tf-chat"></i>
+                                                </span>
+                                                <span class="title">Testimonials</span>
+                                            </span>
+                                            <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
+                                        </a>
+                                    </div>
+                                    <div class="hexagon-item">
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <div class="hex-item">
+                                            <div></div>
+                                            <div></div>
+                                            <div></div>
+                                        </div>
+                                        <a href="contact.html" class="hex-content">
+                                            <span class="hex-content-inner">
+                                                <span class="icon">
+                                                    <i class="tf-envelope2"></i>
+                                                </span>
+                                                <span class="title">Contact</span>
+                                            </span>
+                                            <svg viewbox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
+                                        </a>
+                                    </div>
+                                </div> <!-- /.hexagon-menu -->
 
-// Render navbar
-include "components/navbar.php";
+                            </div> <!-- /.col-xs-12 -->
 
-// Render page splash
-include "components/splash.php";
+                        </div> <!-- /.row -->
+                    </div> <!-- /.container -->
 
-// Build rest of site sections
-foreach ($yamlContent->sections as $section => $value) {
-    if (file_exists("components/sections/" . $value->type . ".php")) {
-        include "components/sections/" . $value->type . ".php";
-    }
-}
-
-// Light / Dark theme toggle
-// include "components/themetoggle.php"
-?>
+                </div> <!-- /.pt-tablecell -->
+            </div> <!-- /.pt-table -->
+        </main> <!-- /.site-wrapper -->
+        
+        <!-- ================================
+        JavaScript Libraries
+        ================================= -->
+        <script src="js/vendor/jquery-2.2.4.min.js"></script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <!-- <script src="js/jquery.easing.min.js"></script> -->
+        <script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/jquery.nicescroll.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery-validation.min.js"></script>
+        <script src="js/form.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ old_index.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use \Dallgoot\Yaml;
+
+// Load content from content.yml
+$yamlContent = Yaml::parseFile("content.yml", 0, 0);
+
+// Include functions
+include "res/php/functions.php";
+
+// Render page head
+include "components/head.php";
+
+// Render navbar
+include "components/navbar.php";
+
+// Render page splash
+include "components/splash.php";
+
+// Build rest of site sections
+foreach ($yamlContent->sections as $section => $value) {
+    if (file_exists("components/sections/" . $value->type . ".php")) {
+        include "components/sections/" . $value->type . ".php";
+    }
+}
+
+// Light / Dark theme toggle
+// include "components/themetoggle.php"
+?>
