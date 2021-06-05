30b1d7c0e4394f81fa8ea5769c3f5145dd883a5c
Aayla Semyonova <aayla@aayla.dev>
2021-06-04 19:06:42 -0600

    Removed JS

--- index.php
+++ index.php
@@ -72,18 +72,5 @@
                 </div> <!-- /.pt-tablecell -->
             </div> <!-- /.pt-table -->
         </main> <!-- /.site-wrapper -->
-        
-        <!-- ================================
-        JavaScript Libraries
-        ================================= -->
-        <script src="js/vendor/jquery-2.2.4.min.js"></script>
-        <script src="js/vendor/bootstrap.min.js"></script>
-        <!-- <script src="js/jquery.easing.min.js"></script> -->
-        <script src="js/isotope.pkgd.min.js"></script>
-        <script src="js/jquery.nicescroll.min.js"></script>
-        <script src="js/owl.carousel.min.js"></script>
-        <script src="js/jquery-validation.min.js"></script>
-        <script src="js/form.min.js"></script>
-        <script src="js/main.js"></script>
     </body>
 </html>
